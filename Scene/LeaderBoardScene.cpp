5d5a38837f1ac492b142afe6ce370942f4f0b8bd
AnanDahal <ananlumphu205@gmail.com>
2025-05-19 03:09:39 +0800

    temp

---
+++ Scene/LeaderBoardScene.cpp
@@ -0,0 +1,43 @@
+//
+// Created by Lenovo on 5/17/2025.
+//
+
+#include <allegro5/allegro_audio.h>
+#include <functional>
+#include <memory>
+#include <string>
+
+#include "Engine/AudioHelper.hpp"
+#include "Engine/GameEngine.hpp"
+#include "Engine/Point.hpp"
+#include "Engine/Resources.hpp"
+#include "PlayScene.hpp"
+#include "Scene/LeaderBoardScene.h"
+#include "UI/Component/ImageButton.hpp"
+#include "UI/Component/Label.hpp"
+#include "UI/Component/Slider.hpp"
+
+
+void LeaderBoardScene::Initialize() {
+    int w = Engine::GameEngine::GetInstance().GetScreenSize().x;
+    int h = Engine::GameEngine::GetInstance().GetScreenSize().y;
+    int halfW = w / 2;
+    int halfH = h / 2;
+
+    Engine::ImageButton *btn;
+
+    btn = new Engine::ImageButton("stage-select/dirt.png", "stage-select/floor.png", halfW - 200, halfH * 3 / 2 - 50, 400, 100);
+    btn->SetOnClickCallback(std::bind(&LeaderBoardScene::BackOnClick, this, 1));
+    AddNewControlObject(btn);
+    AddNewObject(new Engine::Label("Back", "pirulen.ttf", 48, halfW, halfH * 3 / 2, 0, 0, 0, 255, 0.5, 0.5));
+
+    bgmInstance = AudioHelper::PlaySample("shopping_bgm.mp3", true, AudioHelper::BGMVolume);
+}
+void LeaderBoardScene::Terminate() {
+    AudioHelper::StopSample(bgmInstance);
+    bgmInstance = std::shared_ptr<ALLEGRO_SAMPLE_INSTANCE>();
+    IScene::Terminate();
+}
+void LeaderBoardScene::BackOnClick(int stage) {
+    Engine::GameEngine::GetInstance().ChangeScene("stage-select");
+}
