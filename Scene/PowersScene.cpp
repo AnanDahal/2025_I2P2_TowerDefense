f89248d55af1d613a69d51acc6dccbfb799d0889
minghaoma41 <minghaoma41@gmail.com>
2025-06-12 19:22:29 +0800

    Scene to Select Power

---
+++ Scene/PowersScene.cpp
@@ -0,0 +1,43 @@
+#include <allegro5/allegro_audio.h>
+#include <functional>
+#include <memory>
+#include <string>
+
+#include "Engine/AudioHelper.hpp"
+#include "Engine/GameEngine.hpp"
+#include "Engine/Point.hpp"
+#include "Engine/Resources.hpp"
+#include "PowersScene.h"
+#include "Scene/SettingsScene.hpp"
+#include "UI/Component/ImageButton.hpp"
+#include "UI/Component/Label.hpp"
+#include "UI/Component/Slider.hpp"
+
+void PowersScene::Initialize() {
+    int w = Engine::GameEngine::GetInstance().GetScreenSize().x;
+    int h = Engine::GameEngine::GetInstance().GetScreenSize().y;
+    int halfW = w / 2;
+    int halfH = h / 2;
+
+    Engine::ImageButton *btn;
+    btn = new Engine::ImageButton("stage-select/dirt.png", "stage-select/floor.png", halfW - 200, halfH * 3 / 2 - 50, 400, 100);
+    btn->SetOnClickCallback(std::bind(&PowersScene::BackOnClick, this, 2));
+    AddNewControlObject(btn);
+    AddNewObject(new Engine::Label("Back", "pirulen.ttf", 48, halfW, halfH * 3 / 2, 0, 0, 0, 255, 0.5, 0.5));
+
+    bgmInstance = AudioHelper::PlaySample("sass.wav", true, AudioHelper::BGMVolume);
+}
+void PowersScene::Terminate() {
+    AudioHelper::StopSample(bgmInstance);
+    bgmInstance = std::shared_ptr<ALLEGRO_SAMPLE_INSTANCE>();
+    IScene::Terminate();
+}
+
+void PowersScene::Draw() const {
+    al_clear_to_color(al_map_rgb(0, 0, 0));
+    IScene::Draw();
+}
+
+void PowersScene::BackOnClick(int stage) {
+    Engine::GameEngine::GetInstance().ChangeScene("stage-select");
+}
---
+++ Scene/PowersScene.h
@@ -0,0 +1,24 @@
+//
+// Created by user on 6/12/2025.
+//
+
+#ifndef POWERS_H
+#define POWERS_H
+#include <memory>
+
+#include "Engine/IScene.hpp"
+#include <allegro5/allegro_audio.h>
+
+class PowersScene final : public Engine::IScene {
+private:
+    std::shared_ptr<ALLEGRO_SAMPLE_INSTANCE> bgmInstance;
+
+public:
+    explicit PowersScene() = default;
+    void Initialize() override;
+    void Terminate() override;
+    void Draw() const override;
+    void BackOnClick(int stage);
+};
+
+#endif //POWERS_H
