5896e039460832af897dbec6baa9aad8eb657a28
minghaoma41 <minghaoma41@gmail.com>
2025-06-14 17:32:05 +0800

    Music credits

--- Scene/CreditScene.cpp
+++ Scene/CreditScene.cpp
@@ -21,8 +21,20 @@ void CreditScene::Initialize() {
     int halfW = w / 2;
     int halfH = h / 2;
 
-    AddNewObject(new Engine::Label("CREDITS", "pirulen.ttf", 48, halfW, 50, 255, 255, 255, 255, 0.5, 0.5));
-    
+    AddNewObject(new Engine::Label("CREDITS", "pirulen.ttf", 64, halfW, 50, 255, 255, 255, 255, 0.5, 0.5));
+
+    float startY = 130;
+    float lineHeight = 32; // space between lines
+    float nameOffset = 20;
+    float startY2 = 130;
+
+    AddNewObject(new Engine::Label("Music from:", "pirulen.ttf", 48, halfW/2, startY, 255, 255, 255, 255, 0.5, 0));
+    startY += nameOffset;
+    AddNewObject(new Engine::Label("Violet Evergarden OST", "pirulen.ttf", 36, halfW / 2, startY += lineHeight, 200, 200, 200, 255, 0.5, 0));
+    AddNewObject(new Engine::Label("Sousou no Frieren OST", "pirulen.ttf", 36, halfW / 2, startY += lineHeight, 200, 200, 200, 255, 0.5, 0));
+    AddNewObject(new Engine::Label("Oregairu OST", "pirulen.ttf", 36, halfW/2, startY += lineHeight, 200, 200, 200, 255, 0.5, 0));
+    AddNewObject(new Engine::Label("Seishun Buta Yarou OST", "pirulen.ttf", 36, halfW/2, startY += lineHeight, 200, 200, 200, 255, 0.5, 0));
+
     Engine::ImageButton *btn;
     btn = new Engine::ImageButton("stage-select/dirt.png", "stage-select/floor.png", halfW - 200, halfH * 3 / 2 - 50, 400, 100);
     btn->SetOnClickCallback(std::bind(&CreditScene::BackOnClick, this, 2));
